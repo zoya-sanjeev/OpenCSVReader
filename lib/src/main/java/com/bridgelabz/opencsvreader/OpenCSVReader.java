a4d41f72dacd01be2d12b2736f662ebf0dc9402f
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-20 16:32:56 +0530

    ADDED: read from csv file

--- lib/src/main/java/com/bridgelabz/opencsvreader/OpenCSVReader.java
+++ lib/src/main/java/com/bridgelabz/opencsvreader/OpenCSVReader.java
@@ -3,8 +3,28 @@
  */
 package com.bridgelabz.opencsvreader;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import com.opencsv.CSVReader;
+
 public class OpenCSVReader {
-    public boolean someLibraryMethod() {
-        return true;
+    public static final String CSV_FILE_PATH="./users.csv";
+    
+    public static void main(String[] args)throws IOException{
+    	try (
+    		Reader reader=Files.newBufferedReader(Paths.get(CSV_FILE_PATH));
+    		CSVReader csvReader=new CSVReader(reader);
+    	){
+    		String[] nextRecord;
+    		while((nextRecord = csvReader.readNext())!=null) {
+    			System.out.println("Name: "+ nextRecord[0]);
+    			System.out.println("Email: "+ nextRecord[1]);
+    			System.out.println("Phone: "+ nextRecord[2]);
+    			System.out.println("Country: "+ nextRecord[3]);
+    			System.out.println("==================");
+    		}
+    	}
     }
 }
