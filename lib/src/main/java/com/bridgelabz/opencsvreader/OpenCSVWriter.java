5e5038434351d85b8eaf013457b5ebd7bb900af0
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-20 18:18:32 +0530

    ADDED:OpenCSVWriter class created

---
+++ lib/src/main/java/com/bridgelabz/opencsvreader/OpenCSVWriter.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.opencsvreader;
+
+public class OpenCSVWriter {
+
+	private static final String OBJECT_LIST_SAMPLE="./object-list-sample.csv";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
