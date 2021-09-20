e097d362510727f16bfb99f75004a50747044d61
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-20 16:42:35 +0530

    MODIFIED: read from csv all at once

--- lib/src/main/java/com/bridgelabz/opencsvreader/OpenCSVReader.java
+++ lib/src/main/java/com/bridgelabz/opencsvreader/OpenCSVReader.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+
 import com.opencsv.CSVReader;
 
 public class OpenCSVReader {
@@ -17,12 +19,12 @@ public class OpenCSVReader {
     		Reader reader=Files.newBufferedReader(Paths.get(CSV_FILE_PATH));
     		CSVReader csvReader=new CSVReader(reader);
     	){
-    		String[] nextRecord;
-    		while((nextRecord = csvReader.readNext())!=null) {
-    			System.out.println("Name: "+ nextRecord[0]);
-    			System.out.println("Email: "+ nextRecord[1]);
-    			System.out.println("Phone: "+ nextRecord[2]);
-    			System.out.println("Country: "+ nextRecord[3]);
+    		List<String[]> records=csvReader.readAll();
+    		for(String[] record : records) {
+    			System.out.println("Name: "+ record[0]);
+    			System.out.println("Email: "+ record[1]);
+    			System.out.println("Phone: "+ record[2]);
+    			System.out.println("Country: "+ record[3]);
     			System.out.println("==================");
     		}
     	}
